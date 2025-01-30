d07c5d5b708c78e75ad39b8b730673e9bfed1d28
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-30 03:48:34 +0000

    Erb changes

--- dice.rb
+++ dice.rb
@@ -9,18 +9,7 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
 get("/") do
-"
-  <h1>Dice roll, ya filthy animal.</h1>
-  <p></p>
-<ul>
-  
-  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-  <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-  <li><a href=\"/dice/2/10\">Roll two 10-sided die</a></li>
-  <li><a href=\"/dice/5/4\">Roll five 4-sided die</a></li>
- 
-</ul>
-"
+  erb(:elephant)
 end
 
 get ("/giraffe") do
@@ -33,10 +22,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h2>2d6</h2>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -44,19 +32,17 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h2>2d10</h2>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_ten)
 end
 
 get ("/dice/1/20") do
   first_die = rand(1..20)
   
-  outcome = "You rolled a #{first_die}."
+  @outcome = "You rolled a #{first_die}."
 
-  "<h1>1d10</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get ("/dice/5/4") do
@@ -68,8 +54,7 @@ get ("/dice/5/4") do
 
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 end
